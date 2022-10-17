16b5c63e651a056e95d6307330aad2a2198bb83f
Dominika Florczykowska <dflorczy@protonmail.com>
2022-10-17 18:11:55 +0200

    feat: add possibility to add board admins (#21)
    close #17

--- boards/services.py
+++ boards/services.py
@@ -1,4 +1,7 @@
+from typing import List
+
 from django.core.exceptions import ValidationError
+from rest_framework.exceptions import PermissionDenied
 
 from authentication.models import User
 from boards.models import Board
@@ -68,3 +71,29 @@ def add_member_to_board(
     None
     """
     board.members.add(user)
+
+
+def add_admin_to_board(*, board: Board, user: User, users_to_add: List[User]) -> None:
+    """
+    Add users to a board as its admins.
+
+    If a user to add is already an admin, nothing happens. The user to add must be a board member. The adding user
+    must be a board admin.
+
+    Parameters
+    ----------
+    board : Board that the users will be added to as admins.
+    user : user who is performing the action of adding new admins
+    users_to_add : Users that will be added as admins.
+
+    Returns
+    -------
+    None
+    """
+    if user not in board.admins.all():
+        raise PermissionDenied("Only board admin can perform this action.")
+
+    if board.members.filter(id__in=[user.id for user in users_to_add]).count() != len(users_to_add):
+        raise ValidationError({"users_to_add": "Only board members can be added as board admins."})
+
+    board.admins.add(*users_to_add)
--- boards/tests/test_apis.py
+++ boards/tests/test_apis.py
@@ -7,7 +7,7 @@ from rest_framework import status
 from boards.models import Board
 from common.utils import reverse_with_query_params
 from conftest import APIClientWithUser
-from factories import BoardFactory
+from factories import BoardFactory, UserFactory
 
 
 def boards_url(query_kwargs: Optional[Dict[str, Any]] = None) -> str:
@@ -20,6 +20,10 @@ def boards_detail_url(board_id: int) -> str:
 
 def boards_join_url(board_id: int) -> str:
     return reverse("boards:board_detail_join", kwargs={"board_id": board_id})
+
+
+def boards_add_admin_url(board_id: int) -> str:
+    return reverse("boards:board-detail-add-admin", kwargs={"board_id": board_id})
 
 
 @pytest.mark.django_db
@@ -207,5 +211,74 @@ def test_join_board(api_client_with_credentials: APIClientWithUser) -> None:
 @pytest.mark.django_db
 def test_join_board_not_found(api_client_with_credentials: APIClientWithUser) -> None:
     response = api_client_with_credentials.post(boards_join_url(board_id=0))
+
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+@pytest.mark.django_db
+def test_add_admin_to_board(api_client_with_credentials: APIClientWithUser) -> None:
+    board = BoardFactory()
+    board.admins.add(api_client_with_credentials.user)
+    user_to_add = UserFactory()
+    board.members.add(user_to_add)
+
+    response = api_client_with_credentials.post(
+        boards_add_admin_url(board_id=board.id), data={"users_to_add": [user_to_add.id]}
+    )
+
+    assert response.status_code == status.HTTP_200_OK
+    assert list(board.admins.all()) == [api_client_with_credentials.user, user_to_add]
+
+
+@pytest.mark.django_db
+def test_add_admin_who_is_already_an_admin_to_board(api_client_with_credentials: APIClientWithUser) -> None:
+    board = BoardFactory()
+    board.admins.add(api_client_with_credentials.user)
+    user_to_add = UserFactory()
+    board.members.add(user_to_add)
+    board.admins.add(user_to_add)
+
+    response = api_client_with_credentials.post(
+        boards_add_admin_url(board_id=board.id), data={"users_to_add": [user_to_add.id]}
+    )
+
+    assert response.status_code == status.HTTP_200_OK
+    assert list(board.admins.all()) == [api_client_with_credentials.user, user_to_add]
+
+
+@pytest.mark.django_db
+def test_add_admin_who_is_not_a_member_to_board(api_client_with_credentials: APIClientWithUser) -> None:
+    board = BoardFactory()
+    board.admins.add(api_client_with_credentials.user)
+    user_to_add = UserFactory()
+
+    response = api_client_with_credentials.post(
+        boards_add_admin_url(board_id=board.id), data={"users_to_add": [user_to_add.id]}
+    )
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert response.json() == {"users_to_add": ["Only board members can be added as board admins."]}  # type: ignore
+
+
+@pytest.mark.django_db
+def test_add_admin_by_user_that_is_not_an_admin(api_client_with_credentials: APIClientWithUser) -> None:
+    board = BoardFactory()
+    user_to_add = UserFactory()
+
+    response = api_client_with_credentials.post(
+        boards_add_admin_url(board_id=board.id), data={"users_to_add": [user_to_add.id]}
+    )
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+    assert response.json() == {"detail": "Only board admin can perform this action."}  # type: ignore
+
+
+@pytest.mark.django_db
+def test_add_admin_board_not_found(api_client_with_credentials: APIClientWithUser) -> None:
+    user_to_add = UserFactory()
+
+    response = api_client_with_credentials.post(
+        boards_add_admin_url(board_id=0), data={"users_to_add": [user_to_add.id]}
+    )
 
     assert response.status_code == status.HTTP_404_NOT_FOUND
