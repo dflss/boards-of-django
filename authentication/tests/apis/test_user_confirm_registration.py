9da04ef6f23fe8b198d43c440a1709a7b5671f9f
Dominika Florczykowska <dflorczy@protonmail.com>
2023-07-26 08:32:52 -0700

    Add changes after code review

--- authentication/tests/apis/test_user_confirm_registration.py
+++ authentication/tests/apis/test_user_confirm_registration.py
@@ -12,27 +12,29 @@ confirm_registration_url = reverse("authentication:confirm-registration")
 def test_confirm_registration_success(api_client: APIClient) -> None:
     user = UserFactory(is_active=False)
     confirmation_otp = ConfirmationOTPFactory(user=user)
-
     data = {"email": user.email, "otp": confirmation_otp.otp}
 
     response = api_client.post(confirm_registration_url, data)
 
-    user.refresh_from_db()
-
     assert response.status_code == status.HTTP_200_OK
-    assert user.is_active
+
+
+@pytest.mark.django_db
+def test_confirm_registration_invalid_email(api_client: APIClient) -> None:
+    data = {"email": "wrong@example.com", "otp": "wrong_otp"}
+
+    response = api_client.post(confirm_registration_url, data)
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert response.json() == {"email": ["Email is invalid."]}
 
 
 @pytest.mark.django_db
 def test_confirm_registration_invalid_otp(api_client: APIClient) -> None:
     user = UserFactory(is_active=False)
-
     data = {"email": user.email, "otp": "wrong_otp"}
 
     response = api_client.post(confirm_registration_url, data)
 
-    user.refresh_from_db()
-
     assert response.status_code == status.HTTP_400_BAD_REQUEST
     assert response.json() == {"otp": ["One-time password is invalid."]}
-    assert not user.is_active
--- authentication/tests/services/test_user_activate.py
+++ authentication/tests/services/test_user_activate.py
@@ -1,12 +1,22 @@
+import re
+from datetime import timedelta
+
 import pytest
+from django.core.exceptions import ValidationError
+from pytest_mock import MockerFixture
 
 from authentication.services import activate_user
+from boards_of_django import settings
 from factories import ConfirmationOTPFactory, UserFactory
 
 
 @pytest.mark.django_db
-def test_user_becomes_active_after_activation() -> None:
-    user = UserFactory(is_active=False)
+@pytest.mark.parametrize(
+    "is_active",
+    [True, False],
+)
+def test_user_is_active_after_activation(is_active: bool) -> None:
+    user = UserFactory(is_active=is_active)
     confirmation_otp = ConfirmationOTPFactory(user=user)
 
     activate_user(email=user.email, otp=confirmation_otp.otp)
@@ -14,3 +24,42 @@ def test_user_becomes_active_after_activation() -> None:
     user.refresh_from_db()
 
     assert user.is_active
+
+
+@pytest.mark.django_db
+def test_raise_exception_if_user_does_not_exist() -> None:
+    confirmation_otp = ConfirmationOTPFactory()
+
+    with pytest.raises(ValidationError, match=re.escape("{'email': ['Email is invalid.']}")):
+        activate_user(email="wrong@example.com", otp=confirmation_otp.otp)
+
+
+@pytest.mark.django_db
+def test_raise_exception_if_otp_is_invalid() -> None:
+    user = UserFactory(is_active=False)
+    confirmation_otp = ConfirmationOTPFactory()
+
+    with pytest.raises(ValidationError, match=re.escape("{'otp': ['One-time password is invalid.']}")):
+        activate_user(email=user.email, otp=confirmation_otp.otp)
+
+    user.refresh_from_db()
+
+    assert not user.is_active
+
+
+@pytest.mark.django_db
+def test_raise_exception_if_otp_is_expired(mocker: MockerFixture) -> None:
+    user = UserFactory(is_active=False)
+    confirmation_otp = ConfirmationOTPFactory(user=user)
+    mocker.patch(
+        "authentication.services.now",
+        return_value=confirmation_otp.created_at
+        + timedelta(seconds=int(settings.CONFIRMATION_OTP_VALID_FOR_SECONDS) + 1),
+    )
+
+    with pytest.raises(ValidationError, match=re.escape("{'otp': ['One-time password is invalid.']}")):
+        activate_user(email=user.email, otp=confirmation_otp.otp)
+
+    user.refresh_from_db()
+
+    assert not user.is_active
